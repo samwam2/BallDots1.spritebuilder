1b40c1846edce84b9c163587669cb42dbc4d54a2
Sam Showalter <samwam2@gmail.com>
2015-07-15 10:31:05 -0700

    Upload 1 to git
    First upload to github

---
+++ GamePlay.swift
@@ -0,0 +1,28 @@
+//
+//  GamePlay.swift
+//  BallDots
+//
+//  Created by Sam Showalter on 7/14/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class GamePlay: CCNode {
+    //all vars here
+     weak var grid: CCNode!
+    var rows: Int
+    var colomus: Int
+    var spacing: Int
+    
+    init(rows: Int, colomus: Int, spacing: Int) {
+    self.rows = Int()
+    self.colomus = Int()
+    self.spacing = Int()
+    }
+    
+    //all lets here
+    
+    
+    //last closing bracket all new code ^ 
+}
---
+++ Source/MainScene.swift
@@ -0,0 +1,19 @@
+//
+//  GamePlay.swift
+//  BallDots
+//
+//  Created by Sam Showalter on 7/14/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+import Foundation
+import UIKit
+class MainScene: CCNode {
+    //all vars here
+    
+    
+    //all lets here
+    
+    
+    
+    //last closing bracket all new code ^
+}
